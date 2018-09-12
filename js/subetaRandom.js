b4f5074f8541b73c498c623d23f29f9e46cff8bb
Christine Loong <34012305+Ceredin@users.noreply.github.com>
2018-09-12 21:40:38 +1200

    Create subetaRandom.js

---
+++ js/subetaRandom.js
@@ -0,0 +1,89 @@
+ var speciesA = ["Montre", 
+        "Kumos",
+        "Keeto",
+        "Dragarth",
+        "Legeica", 
+        "Telenine"
+        ];
+        
+        var coloursA = ["Angelic",
+        "Aqua",
+        "Arid",
+        "Blacklight",
+        "Bloodred",
+        "Cherry",
+        "Chibi",
+        "Common",
+        "Cream",
+        "Darkmatter",
+        "Dawn",
+        "Dusk",
+        "Field",
+        "Galactic",
+        "Glacier",
+        "Glade",
+        "Gold",
+        "Graveyard",
+        "Harvest",
+        "Hydrus",
+        "Lilac",
+        "Marsh",
+        "Nightmare",
+        "Nostalgic",
+        "Nuclear",
+        "Reborn",
+        "Scribble",
+        "Silver",
+        "Spectrum",
+        "Steamwork",
+        "Storm",
+        "Sun",
+        "Sweetheart",
+        "Twilight",
+        "Vibrant"
+        ]
+        
+        var specNum = "";
+        var colNum = "";
+        var spec ="";
+        var col ="";
+        
+        function generate() {
+                genSpec();
+                genCol();
+        }
+        
+        function genSpec() {
+                getSpec();
+                spec = speciesA[specNum];
+            document.getElementById("demo0").innerHTML = spec;
+                display();
+        }
+        
+        function genCol() {
+                getCol();
+                col = coloursA[colNum].toString();
+            document.getElementById("demo1").innerHTML = col;
+                display();
+        }
+        
+        function display(){
+        if (col == "Common"){
+                document.getElementById("petPic").src = "https://img.subeta.net/pets/" + spec.toLowerCase() + ".png";
+        }
+        else{
+                document.getElementById("petPic").src = "https://img.subeta.net/pets/" + spec.toLowerCase() + "_" + col.toLowerCase() + ".png";
+        }
+        }
+        
+        //this function gets a random index from the speciesA array
+        function getSpec(){
+                    specNum = Math.floor(Math.random()*speciesA.length);
+        
+        }
+        
+        //this function gets a random index from the coloursA array
+        function getCol(){
+                    colNum = Math.floor(Math.random()*coloursA.length);
+        
+        }
